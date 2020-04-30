4eb30e4a03e64543afa0e6f39431f85ed3916f4a
meyash <yashsn2127@gmail.com>
2020-04-30 21:29:07 +0530

    prim_kruskal

---
+++ 18_graphs/2_prim.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//concentrate on making tree and always select min edge
+
+int I =INT_MAX; //infinity --set it to max int
+int cost[][8]={
+    {I,I,I,I,I,I,I,I},
+
+    {I,     I,25,I,I,I,5,I},
+    {I,     25,I,12,I,I,I,10},
+    {I,     I,12,I,8,I,I,I},
+    {I,     I,I,8,I,16,I,14},
+    {I,     I,I,I,16,I,20,18},
+    {I,     5,I,I,I,20,I,I},
+    {I,     I,10,I,14,18,I,I}
+};
+int near[8]={I,I,I,I,I,I,I,I};
+int t[2][6];
+
+int main(){
+    int min=I;
+    int n=7;
+    int u,v; //vertices connecting an edge
+
+    //STEP1
+    //finding min
+    for(int i=1;i<=n;i++){
+        for(int j=i;j<=n;j++){
+            if(cost[i][j]<min){
+                min=cost[i][j];
+                u=i; //saves value 1
+                v=j; //saves value 6
+            }
+        }
+    }
+    //saving edges to "t" and seeting 1 and 6 in "near" as 0
+    t[0][0]=u;
+    t[1][0]=v;
+    near[u]=near[v]=0; //0 means used in MST already
+    // updating edge is near to 6 or 1 in "near"
+    for(int i=1;i<=n;i++){
+        if(near[i]!=0){
+            if(cost[i][u]<cost[i][v]){
+                near[i]=u;
+            }else{
+                near[i]=v;
+            }
+        }
+    }
+
+    //STEP2 --REPEATING STEP
+    int k=0;
+    for(int i=1;i<n-1;i++){
+        min=I; //resetting min to infinity
+
+        // finding next minimum from remaining
+        for(int j=1;j<=n;j++){
+            if(near[j]!=0&&cost[j][near[j]]<min){
+                k=j;
+                min=cost[j][near[j]];
+            }
+        }
+        //savin min edge and making used nodes in "near" 0
+        t[1][i]=k;
+        t[0][i]=near[k];
+        near[k]=0;
+        //updating near
+        for(int j=1;j<=n;j++){
+            if(near[j]!=0&&cost[j][k]<cost[j][near[j]]){
+                near[j]=k;
+            }
+        }
+    }
+
+    // print t
+    
+    // for(int i=0;i<n-1;i++)
+    // {
+    //     cout<<"("<<t[0][i]<<","<<t[1][i]<<")"<<endl;
+    // }
+    int m=0;
+    for(m=0;m<n-1;m++){
+        cout<<"("<<t[0][m]<<")->";
+    }
+    m--;
+    cout<<"("<<t[1][m]<<")"<<endl;
+
+}
+
---
+++ 18_graphs/3_kruskal.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//concentrate on selecting minimum and make a tree
+
+int I =INT_MAX; //infinity --set it to max int
+int edges[3][9]={
+    {1, 1, 2, 2, 3, 4, 4, 5, 5},
+    {2, 6, 3, 7, 4, 5, 7, 6, 7},
+    {25, 5, 12, 10, 8, 16, 14, 20, 18}
+};
+int myset[8]={-1,-1,-1,-1,-1,-1,-1,-1}; //used to detect cycles
+int included[9]={0}; //to avoid repetition
+int t[2][6]; //to store solution
+
+//to join parents
+void unionfn(int u,int v){
+    if(myset[u]<myset[v]){
+        myset[u]=myset[u]+myset[v];
+        myset[v]=u;
+    }else{
+        myset[v]=myset[u]+myset[v];
+        myset[u]=v;
+    }
+}
+
+//to find parents //collapsing find
+int findfn(int u){
+    int x=u;
+    int v=0;
+    while(myset[x]>0){
+        x=myset[x];
+    }
+    while(u!=x){
+        v=myset[u];
+        myset[u]=x;
+        u=v;
+    }
+    return x;
+}
+
+int main(){
+    //in kruskal every step is recursive
+
+    int n=7; //no of vertices
+    int e=9; //no of edges
+    int i,j,k,u,v;
+    i=j=k=0;
+    int min=I;
+
+    while(i<n-1){
+        min=I;
+        //find min
+        for(j=0;j<e;j++){ //searching in array edges
+            if(included[j]==0&&edges[2][j]<min){
+                min=edges[2][j];
+                k=j;
+                // u&v are vertice names
+                u=edges[0][j];
+                v=edges[1][j];
+            }
+        }
+
+        //check cycle :  then include in solution if no cycle
+        if(findfn(u)!=findfn(v)){ //i.e if parents are not same
+            t[0][i]=u;
+            t[1][i]=v;
+            //joining parents
+            unionfn(findfn(u),findfn(v)); //making 1 as parent of other // joining
+            i++;
+        }
+        included[k]=1; 
+
+    }
+
+    // print t
+    int m=0;
+    for(m=0;m<n-1;m++){
+        cout<<"("<<t[0][m]<<")->"<<"("<<t[1][m]<<")"<<endl;
+    }
+
+}
