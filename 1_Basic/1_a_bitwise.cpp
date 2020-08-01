f9bbe90b6c3ea98aa25c21994936a62b01127b95
meyash <yashsn2127@gmail.com>
2020-08-01 22:24:43 +0530

    _upd

--- 1_Basic/1_a_bitwise.cpp
+++ 1_Basic/1_a_bitwise.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main(){
     int a=1;
-    cout<<(a<<1)<<endl;
-    cout<<(a<<2)<<endl;
-    cout<<(a<<3)<<endl;
-    cout<<(a<<4)<<endl;
-    cout<<(a<<5)<<endl;
+    cout<<"a<<1 = "<<(a<<1)<<endl;
+    cout<<"a<<2 = "<<(a<<2)<<endl;
+    cout<<"a<<3 = "<<(a<<3)<<endl;
+    cout<<"a<<4 = "<<(a<<4)<<endl;
+    cout<<"a<<5 = "<<(a<<5)<<endl;
 }
